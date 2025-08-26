13c0fc1ab8270549b70087ca964ff529b0966948
RobsiZocktUni <184125521+RobsiZocktUni@users.noreply.github.com>
2025-08-26 15:23:57 +0200

    massive asset upload, all heros most maps

---
+++ server/app/p/img/indexer.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+// Change this to the folder you want to index
+const folderPath = './maps';
+const outputFile = 'fileIndex.json';
+
+function indexFiles(folder) {
+  const files = fs.readdirSync(folder, { withFileTypes: true });
+  const fileIndex = {};
+
+  let counter = 1;
+  files.forEach(file => {
+    if (file.isFile()) {
+      fileIndex[counter] = {
+        name: path.parse(file.name).name, // filename without extension
+        path: path.join(folder, file.name)
+      };
+      counter++;
+    }
+  });
+
+  return fileIndex;
+}
+
+const indexedFiles = indexFiles(folderPath);
+
+// Write the JSON to a file
+fs.writeFileSync(outputFile, JSON.stringify(indexedFiles, null, 2));
+
+console.log(`Indexed ${Object.keys(indexedFiles).length} files. Saved to ${outputFile}`);
