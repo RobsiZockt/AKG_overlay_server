b87c1b3210365ba73ea56700465b6e0ba05f1d89
RobsiZocktUni <184125521+RobsiZocktUni@users.noreply.github.com>
2025-08-27 12:18:10 +0200

    added maps and heros to api request

--- server/app/server.js
+++ server/app/server.js
@@ -4,7 +4,9 @@ const path = require("path");
 const app = express();
 const PORT = 4000;
 
-const mapsFile = path.join(__dirname, "api","played_maps.json");
+const playedmaps = path.join(__dirname, "api","played_maps.json");
+const maps = path.join(__dirname, "api","maps.json");
+const heros = path.join(__dirname, "api","heros.json");
 
 //middleware for parsing json files
 app.use(express.json());
@@ -20,9 +22,9 @@ app.get("/api/played_maps/stream", (req,res) =>{
     let lastMtime = 0;
  const sendFile = () => {
     try {
-      const stats = fs.statSync(mapsFile);
+      const stats = fs.statSync(playedmaps);
       if (stats.mtimeMs !== lastMtime && stats.size > 0) {
-        const obj = JSON.parse(fs.readFileSync(mapsFile,"utf8"));
+        const obj = JSON.parse(fs.readFileSync(playedmaps,"utf8"));
         res.write(`data: ${JSON.stringify(obj)}\n\n`);
         lastMtime = stats.mtimeMs;
       }
@@ -43,7 +45,7 @@ app.get("/api/played_maps/stream", (req,res) =>{
 app.post('/api/played_maps', (req, res) => {
   try {
     // Read existing JSON
-    const json = JSON.parse(fs.readFileSync(mapsFile, 'utf-8'));
+    const json = JSON.parse(fs.readFileSync(playedmaps, 'utf-8'));
 
    const entryKey = req.body.key;
     if (!entryKey || !json[entryKey]) return res.status(400).json({ error: "Invalid key" });
@@ -51,7 +53,7 @@ app.post('/api/played_maps', (req, res) => {
     delete json[entryKey].key;
 
     // Write updated JSON back to file
-    fs.writeFileSync(mapsFile, JSON.stringify(json, null, 2));
+    fs.writeFileSync(playedmaps, JSON.stringify(json, null, 2));
 
     res.json({ status: 'ok', latest: json[entryKey] });
   } catch (err) {
@@ -64,15 +66,30 @@ app.post('/api/played_maps', (req, res) => {
 
 
 
+app.get("/api/maps", (req,res)=>{
+  try{
+    const data = fs.readFileSync(maps, "utf8");
+    res.json(JSON.parse(data));
+  }catch(err){
+    res.status(500).json({error: "Could not read maps.json"});
+  }
+});
 
-
+app.get("/api/heros", (req,res)=>{
+  try{
+    const data = fs.readFileSync(heros, "utf8");
+    res.json(JSON.parse(data));
+  }catch(err){
+    res.status(500).json({error: "Could not read heros.json"});
+  }
+});
 
 
 
 /////////LEGACY GET / POST ////////////////////////
 app.get("/api/played_maps", (req,res) =>{
 try{
-    const data = fs.readFileSync(mapsFile,"utf8");
+    const data = fs.readFileSync(playedmaps,"utf8");
     res.json(JSON.parse(data));
 }catch (err){
     console.error("Error reading played_maps.json:", err);
@@ -83,4 +100,4 @@ try{
 ////////////////LEGACY END ///////////////////////////
 app.listen(PORT,()=>
 console.log("Server is listening on ${PORT}"));
-console.log("Maps file path:", mapsFile);
+console.log("Maps file path:", playedmaps);
