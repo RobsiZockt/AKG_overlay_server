723b4b6d472f4b92b79ad76d6e12ec14e39bc494
RobsiZocktUni <184125521+RobsiZocktUni@users.noreply.github.com>
2025-08-28 13:07:54 +0200

    added api calls and save button

--- server/app/p/js/m_setup.js
+++ server/app/p/js/m_setup.js
@@ -13,9 +13,19 @@ function waitForContainer(id, callback) {
 
 
 let matchup_data ={};
-let container;
+
 
 waitForContainer("##setup", (container) => {
+
+(async function fetchData() {
+  try {
+    const response = await fetch("/api/matchup");
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    myData = await response.json();
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  }
+})();
 
     function createSetup(){
   // Find popup content container
@@ -24,7 +34,7 @@ waitForContainer("##setup", (container) => {
     container.innerHTML = "";
 
   container.style.display = "grid";
-  container.style.gridTemplateColumns = "33% 33% 33%";
+  container.style.gridTemplateColumns = "66% 33%";
   container.style.gridTemplateRows = "auto";
 
   const settergrid = document.createElement("div");
@@ -42,6 +52,30 @@ settergrid.appendChild(blue_name_container);
 
 const red_name_container = createInput("Red Team", "red",2,1);
 settergrid.appendChild(red_name_container);
+
+const blue_img_container = createInput("Blue Logo", "blue_logo",1,2);
+settergrid.appendChild(blue_img_container);
+
+const red_img_container = createInput("Red Logo", "red_logo",2,2);
+settergrid.appendChild(red_img_container);
+
+
+
+const save = document.createElement("button");
+
+  save.textContent = "SAVE";
+save.style.backgroundColor = "#ffee00"; // blue background
+save.style.color = "#fff";              // white text
+save.style.border = "none";
+save.style.borderRadius = "4px";
+save.style.cursor = "pointer";
+save.style.fontSize = "14px";
+save.style.padding = "8px 16px";
+  save.addEventListener("click", () => handleSave());
+  save.style.gridColumn ="2";
+  container.appendChild(save);
+
+
 
   }
 
@@ -73,12 +107,40 @@ settergrid.appendChild(red_name_container);
   }
 
   function handleEdit(id, value){
-    const edit = {[id]: value}
+    matchup_data[id]= value;
   }
+
+  function handleSave(){
+    matchup_data["blue_score"]= 0;
+    matchup_data["red_score"] = 0;
+
+    newMatchup(matchup_data);
+
+    }
+
+async function newMatchup(data) {
+
+      try {
+    const response = await fetch("/api/new_matchup",{
+      method: "POST",
+      headers: {"Content-Type": "application/json",},
+      body:JSON.stringify(data),
+    });
+    if(!response.ok) throw new Error("Network response was not ok");
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error){
+    console.error("Error setting up new Matchup: ", error)
+  }
+}
+
 }
 
 createSetup();
 });
+
+
 
 })();
 
