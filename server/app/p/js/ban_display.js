1c68ecfdeaced76effeceb596cf6c77705fdddcb
RobsiZocktUni <184125521+RobsiZocktUni@users.noreply.github.com>
2025-09-14 03:36:59 +0200

    finished ban display logic

--- server/app/p/js/ban_display.js
+++ server/app/p/js/ban_display.js
@@ -29,23 +29,43 @@ function waitForContainer(id, callback) {
 
 
 	function BanDisplay(){
-    const [maps, setMaps] = useState([]);
+    const [lastmaps, setLastMaps] = useState({});
+
     useEffect(() => {
       document.addEventListener("playedMapsUpdate", (e) => {
         try {
           const data = e.detail; // already a parsed JS object
-          setMaps(Object.entries(data)); // update state or variable
+					const lastKey = Math.max(...Object.keys(data).map(Number));
+					console
+					setLastMaps(data[lastKey]); // update state or variable
         } catch (err) {
           console.error("Error handling playedMapsUpdate:", err);
         }
       });
     }, []);
 
+
+	console.log(lastmaps.ban_red);
 	
 		return h("div",{className: "flex h-[300px] w-[230px] flex-col"},
-			h("div", { className: "flex h-[15%] items-center justify-start pl-2", style: { clipPath: "polygon(0 0, 100% 0, 96% 100%, 0 100%)", backgroundColor: "#35a653ff" }}),
+		h("div", { className: "flex h-[15%] items-center justify-start pl-2 shadow-2xl", style: { clipPath: "polygon(0 0, 100% 0, 96% 100%, 0 100%)", backgroundColor: "#35a653ff" }},
+			h("div",{ className: "text-3xl items-center justify-start pl-4"},"Bans:"),
+		),
 		h("div", { className: "flex h-[85%] items-center justify-start pl-2", style: { clipPath: "polygon(0 0, 90% 0, 70% 100%, 0 100%)", backgroundColor: "#575757ff" } },
-			
+			h("div",{className:"flex-1 h-full flex-col items-center justify-start p-4"},
+				h("div",{className:"h-[45%] w-full  flex"},
+					h("div",{className:"h-full w-[5%] bg-red-700"}),
+					h("div",{className:"h-full w-[5%] bg-transparent"}),
+					h("img",{className:"flex h-full aspect-square object-cover bg-[#808080]", src:lastmaps.ban_blue}, ),
+				),
+				h("div",{className:"flex-1 h-[7%] w-full"}),
+				h("div",{className:"h-[45%] w-full flex flex-1"},
+					h("div",{className:" h-full w-[5%] bg-[#00a2d4]"}),
+					h("div",{className:"h-full w-[5%] bg-transparent"}),
+					h("img",{className:"flex h-full aspect-square object-cover bg-[#808080]", src:lastmaps.ban_red}, ),
+				)
+
+			)
 		),
 		)
 	}
