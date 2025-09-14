7f2946cbe427fb6b42fe1080d4a19b2ac8a05f45
RobsiZocktUni <184125521+RobsiZocktUni@users.noreply.github.com>
2025-09-14 16:56:15 +0200

    changes text sizes

--- server/app/p/js/matchup_stats.js
+++ server/app/p/js/matchup_stats.js
@@ -44,13 +44,13 @@ function MatchupStats(){
 		h("span",{className:"w-full text-[50px] text-white"},"Aktueller Spielstand:"),
 		h("div",{id:"T1",className:"flex w-full h-[50%] justify-center"},
 			h("img",{id:"t1_logo",className:"h-auto aspect-square object-cover",src:matchup.blue_logo}),
-			h("span", { className: "font-arial text-white w-full text-[50px] pl-2" }, matchup.blue),
-			h("span", { className: "ml-2 font-arial text-[50px] text-white items-center justify-center" }, matchup.blue_score)
+			h("span", { className: "font-arial text-white w-full text-[40px] pl-2" }, matchup.blue),
+			h("span", { className: "ml-2 font-arial text-[40px] text-white items-center justify-center" }, matchup.blue_score)
 		),
 		h("div",{id:"T1",className:"flex w-full h-[50%]"},
 			h("img",{id:"t1_logo",className:"h-auto aspect-square object-cover",src:matchup.red_logo}),
-			h("span", { className: "font-arial text-white w-full text-[50px] pl-2" }, matchup.red),
-			h("span", { className: "ml-2 font-arial text-[50px] text-white items-center justify-center" }, matchup.red_score)
+			h("span", { className: "font-arial text-white w-full text-[40px] pl-2" }, matchup.red),
+			h("span", { className: "ml-2 font-arial text-[40px] text-white items-center justify-center" }, matchup.red_score)
 		)
 	)
 }
