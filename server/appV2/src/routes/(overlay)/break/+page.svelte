111ae521b802064a50580266b2da85c27dd7ec00
robsizockt <l.m.hartmann03@gmail.com>
2025-12-22 16:09:55 +0100

    Svelte and node docker can now comunicate, imgs now get served through nginx

--- server/appV2/src/routes/(overlay)/break/+page.svelte
+++ server/appV2/src/routes/(overlay)/break/+page.svelte
@@ -1,6 +1,29 @@
 <script>
-  // Any page-specific logic here
+  import { onMount } from 'svelte';
+
+  let apiResults;
+
+  const URL = '/api/maps';
+
+onMount(() => {
+  fetch('/api/api/maps')
+    .then((response) => {
+      console.log("STATUS:", response.status);
+      console.log("STATUS TEXT:", response.statusText);
+      return response.text();
+    })
+    .then((text) => {
+      console.log("RAW RESPONSE:", text);
+    })
+    .catch(console.error);
+});
+
 </script>
 
-<h3>Overlay — Break Page</h3>
-<p>This is a public read-only page.</p>
+<main>
+  <div style="white-space: pre-wrap; margin-top: 1rem;">
+    {#if apiResults}
+      {JSON.stringify(apiResults, null, 2)}
+    {/if}
+  </div>
+</main>
