9b5c83d5cf205569ff072fa3c238c0437f28f07d
robsizockt <l.m.hartmann03@gmail.com>
2026-01-08 22:16:54 +0100

    added font "Teko" to this app and tailwind
    ussage tw: font-[teko]

--- server/appV2/src/routes/+layout.svelte
+++ server/appV2/src/routes/+layout.svelte
@@ -1,26 +1,37 @@
-<!-- src/routes/+layout.svelte -->
 <script>
-  export let data
+	import './layout.css';
+ 	import "../app.css";
+	export let data;
+	//import {sse} from '$lib/stores/createSSEStore'
+
+  import { onMount, onDestroy } from 'svelte';
+  
+  onMount(() => {;
+	//sse.connect();
+  });
+
+  onDestroy(() => {
+  });
+
+
 </script>
 
+<!-- src/routes/+layout.svelte -->
+
 {#if data.isCaster}
-  <div style="border: 3px solid red; padding: 1rem;">
-    <h2>Admin Layout</h2>
-    <slot />
-  </div>
+	<div style="border: 3px solid red; padding: 1rem;">
+		<h2>Admin Layout</h2><slot></slot>
+	</div>
 {:else if data.isPublic}
-  <div style="border: 3px solid green; padding: 1rem;">
-    <h2>Public Layout</h2>
-    <slot />
-  </div>
-  {:else if data.isOverlay}
-  <div style="border: 3px solid blue; padding: 1rem;">
-    <h2>Overlay Layout</h2>
-    <slot />
-  </div>
+	<div style="border: 3px solid green; padding: 1rem;">
+		<h2>Public Layout</h2><slot></slot>
+	</div>
+{:else if data.isOverlay}
+	<div style="border: 3px solid blue; padding: 1rem;">
+		<h2>Overlay Layout</h2><slot></slot>
+	</div>
 {:else}
-  <div style="border: 3px solid gray; padding: 1rem;">
-    <h2>Unknown host</h2>
-    <slot />
-  </div>
+	<div style="border: 3px solid gray; padding: 1rem;">
+		<h2>Unknown host</h2><slot></slot>
+	</div>
 {/if}
