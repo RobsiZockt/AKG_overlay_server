7f45b2f27ae12a96016d17de460ac342b26c23da
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-01-28 23:30:26 +0100

    Added Facecam assets

---
+++ server/appV2/src/lib/assets/Facecam.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let name;
+    export let social;
+    export let no_vid=false;
+    export let img;
+
+    import Background from "./Background.svelte";
+</script>
+
+<div class="h-[720px] w-[1213px] flex justify-center">
+  <!-- Shared width wrapper -->
+  <div class="h-full w-fit flex flex-col">
+
+    <!-- Cam + Sides -->
+    <div class="flex h-[88%]">
+      <div class="h-full w-[10px] bg-[#348333]"></div>
+
+      <div class="h-full w-[1193px]">
+        {#if no_vid === true}
+          <Background Map_url={img} />
+        {/if}
+      </div>
+
+      <div class="h-full w-[10px] bg-[#348333]"></div>
+    </div>
+
+    <!-- Cam Footer -->
+    <div class="h-[12%] w-full text-center bg-[#000000ee] flex flex-col justify-center">
+      <p class="text-5xl text-white tracking-wide">{name}</p>
+      <p class="text-2xl text-white tracking-wide">{social}</p>
+    </div>
+
+  </div>
+</div>
+
