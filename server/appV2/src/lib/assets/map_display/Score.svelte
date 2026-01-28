410c32a7aeed252921f2542b320f2d81363a3b38
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-01-28 23:31:19 +0100

    edited Score to work on 2k

--- server/appV2/src/lib/assets/map_display/Score.svelte
+++ server/appV2/src/lib/assets/map_display/Score.svelte
@@ -14,10 +14,10 @@
     
     </script>
 
-    <div class="relative h-[100px] flex items-center justify-center w-full top-9/16">
+    <div class="relative h-[100px] flex items-center justify-center w-full top-2/3">
         <div class=" flex items-center justify-center rounded-lg"><!--bg-[#000000aa] pr-2 pt-2-->
-            <span class="ml-2 drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.8)] font-[teko] text-5xl text-white items-center justify-center filter {winner === "red" ? "brightness-50" : ""}">{blue}</span>
-            <span class="ml-2 drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.8)] font-[teko] text-5xl text-white items-center justify-center">:</span>
-            <span class="ml-2 drop-shadow-[0_1.4px_1.4px_rgba(0,0,0,0.8)] font-[teko] text-5xl text-white items-center justify-center filter {winner === "blue" ? "brightness-50" : ""}">{red}</span>
+            <span class="ml-2 drop-shadow-[0_4px_4px_rgba(0,0,0,0.8)] font-[teko] text-7xl text-white items-center justify-center filter {winner === "red" ? "brightness-50" : ""}">{blue}</span>
+            <span class="ml-2 drop-shadow-[0_4px_4px_rgba(0,0,0,0.8)] font-[teko] text-7xl text-white items-center justify-center">:</span>
+            <span class="ml-2 drop-shadow-[0_4px_4px_rgba(0,0,0,0.8)] font-[teko] text-7xl text-white items-center justify-center filter {winner === "blue" ? "brightness-50" : ""}">{red}</span>
         </div>
     </div>
