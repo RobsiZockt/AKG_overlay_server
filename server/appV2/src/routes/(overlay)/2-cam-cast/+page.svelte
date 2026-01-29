96d56285aa0f5359863bb821a56d892ee6afe592
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-01-29 15:24:33 +0100

    fixed 2-cam scene to work with animations

--- server/appV2/src/routes/(overlay)/2-cam-cast/+page.svelte
+++ server/appV2/src/routes/(overlay)/2-cam-cast/+page.svelte
@@ -1,10 +1,11 @@
 <script>
  import Facecam from "$lib/assets/Facecam.svelte";
+
 </script>
 
 <div class="h-[100vh] w-full p-10 flex items-center justify-center bg-transparent">
-<Facecam name="RobsiZockt" social="robsizockt" img="" no_vid={false}></Facecam> 
+    <Facecam name="RobsiZockt" social="robsizockt" img="" no_vid={false}></Facecam> 
 <!--SPACER-->
-<div class="w-[7%] h-full"></div>
+<div class="w-[2%] h-full"></div>
 <Facecam name="AHAB" social="ahabini" img="" no_vid={false}></Facecam>
 </div>
