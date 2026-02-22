34c0ffdfae7375b1182266b36e30af3f5374f936
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-02-22 22:39:16 +0100

    added nav bar

--- server/appV2/src/routes/(cast)/control/+page.svelte
+++ server/appV2/src/routes/(cast)/control/+page.svelte
@@ -1,16 +1,35 @@
 <script>
     import Timer from "$lib/assets/cast/timer.svelte";
     import Header from "$lib/assets/header.svelte";
+    import Navbar from "$lib/assets/cast/navbar.svelte";
+    
+  let current_tag = "Live";
+  let updateData = (newData) => {
+    current_tag =newData;
+  }
 </script>
 
 <div class="w-full h-[100vh] bg-[#2b2d30]">
-<div class ="w-full h-fit">
-  <div class="absolute inset-0 z-10">
-    <Header/>
-  </div> 
-
-  <div class="absolute inset-0 z-20">
-    <Timer/>
+  <div class ="w-full h-[68px] relative">
+    <div class="absolute inset-0">
+      <Header/>
+    </div> 
+    <div class="absolute inset-0 z-2">
+      <Timer/>
+    </div>
   </div>
-</div>
+
+<Navbar
+  current_tab={updateData}
+ 
+/>
+
+{#if current_tag === "Live"}
+  <p>Home content</p>
+{:else if current_tag === "Setup"}
+  <p>Products content</p>
+{:else if current_tag ==="Teams"}
+  <p>Contact content</p>
+{/if}
+
 </div>
