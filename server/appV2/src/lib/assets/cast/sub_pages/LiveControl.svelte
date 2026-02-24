2f442fd95b2aea7497316687fa9109779adc7ed7
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-02-24 20:27:59 +0100

    live cnorol subsite for easy management

---
+++ server/appV2/src/lib/assets/cast/sub_pages/LiveControl.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    import ImgPicker from "$lib/assets/cast/ImgPicker.svelte";
+
+      import { onMount } from "svelte";
+  import { maps } from "$lib/stores/maps";
+
+  onMount(()=>{maps.load()});
+
+  $: console.log($maps);
+
+  
+</script>
+
+
+{#if $maps.length == 0}
+  <p>Loading…</p>
+{:else if $maps.length != 0}
+ <ImgPicker items={$maps} selectedItem={}/>
+ {/if}
