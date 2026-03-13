25f893dcad4d6c5dd5acfa006fce7dbe31e02f84
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-13 16:12:25 +0100

    added scorecontrol element

---
+++ server/appV2/src/lib/assets/cast/ScoreControl.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { playedMaps } from "$lib/stores/playedMapsUpdate";
+    import Button from "../Button.svelte";
+</script>
+
+<Button text={"+"} apiPath={'/api/api/played_maps/1/blue/add'} method={"POST"}/>
