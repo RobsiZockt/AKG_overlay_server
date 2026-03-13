251cdf7ea67605d220edc5d6a40d9fd4f13b902a
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-13 17:35:04 +0100

    included the current map score it works, just dont ask why, cauz idk

--- server/appV2/src/lib/assets/cast/ScoreControl.svelte
+++ server/appV2/src/lib/assets/cast/ScoreControl.svelte
@@ -1,6 +1,38 @@
 <script>
     import { playedMaps } from "$lib/stores/playedMapsUpdate";
+		import { matchupData } from '$lib/stores/matchupData';
     import Button from "../Button.svelte";
+  import Score from "../map_display/Score.svelte";
+
+	let curr_map = 2;
+
+		 $: entries = Object.entries($playedMaps ?? {})
+    .map(([key, value]) => ({
+      id: Number(key),
+      ...value
+    }))
+    .sort((a, b) => a.id - b.id);
+
+	$:	console.log(entries);
+	$:	console.log(typeof(entries?.[1]?.score_blue));
 </script>
 
-<Button text={"+"} apiPath={'/api/api/played_maps/1/blue/add'} method={"POST"}/>
+{#if entries?.length == 0}
+  <p>Loading…</p>
+{:else if entries?.length != 0}
+<div class="flex">
+	<div>
+		<Button text={"+"} apiPath={`/api/api/played_maps/${curr_map}/blue/add`} method={"POST"}/>
+		<span>{$matchupData.blue_short}</span>
+		<Button text={"-"} apiPath={`/api/api/played_maps/${curr_map}/blue/sub`} method={"POST"}/>
+	</div>
+	<div>
+	<Score blue_score={entries?.[curr_map-1]?.score_blue} red_score={entries?.[curr_map-1]?.score_red}/>
+	</div>
+		<div>
+		<Button text={"+"} apiPath={`/api/api/played_maps/${curr_map}/red/add`} method={"POST"}/>
+		<span>{$matchupData.blue_short}</span>
+		<Button text={"-"} apiPath={`/api/api/played_maps/${curr_map}/red/sub`} method={"POST"}/>
+	</div>
+</div>
+{/if}
