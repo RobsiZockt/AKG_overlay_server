a97ad5254d0df354c2af602fd457ebc1956d7a99
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-14 18:40:51 +0100

    fixed div height

--- server/appV2/src/lib/assets/map_display/Score.svelte
+++ server/appV2/src/lib/assets/map_display/Score.svelte
@@ -22,7 +22,7 @@ pos ="top-2/3";
     
     </script>
 
-    <div class='relative h-[100px] flex items-center justify-center w-full {pos}'>
+    <div class='relative h-[74px] flex items-center justify-center w-full {pos}'>
         <div class=" flex items-center justify-center rounded-lg"><!--bg-[#000000aa] pr-2 pt-2-->
             <span class="ml-2 drop-shadow-[0_4px_4px_rgba(0,0,0,0.8)] font-[teko] text-7xl text-white items-center justify-center filter {winner === "red" ? "brightness-50" : ""}">{blue}</span>
             <span class="ml-2 drop-shadow-[0_4px_4px_rgba(0,0,0,0.8)] font-[teko] text-7xl text-white items-center justify-center">:</span>
