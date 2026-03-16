4d38478bb7f25582548f9fa24fc995dc0b87d935
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-17 00:08:03 +0100

    Textfield component

---
+++ server/appV2/src/lib/assets/cast/Textfield.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { placeholdertxt = "", value = $bindable(""), width=100, height=50} = $props();
+    
+
+</script>
+
+<input class="flex p-3 text-base" style="width: {width}px; height: {height}px;" placeholder={placeholdertxt.toString()} bind:value/>
