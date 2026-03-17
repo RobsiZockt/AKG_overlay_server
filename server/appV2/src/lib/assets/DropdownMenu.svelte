0a37fde16a9a6bc9923951b3f89db04c636a9201
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-17 16:42:32 +0100

    Dropdown menu component

---
+++ server/appV2/src/lib/assets/DropdownMenu.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { list=[], nulltext="", value = $bindable(0), width=100, height=50 } = $props();
+</script>
+
+<select bind:value class="flex p-3 text-base" style="width: {width}px; height: {height}px;">
+  <option value={null} disabled>{nulltext}</option>
+
+  {#each list as item}
+    <option value={item.id}>
+      {item.name}
+    </option>
+  {/each}
+</select>
