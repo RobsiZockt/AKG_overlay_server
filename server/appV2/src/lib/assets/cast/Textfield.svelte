f3a89b29df0f7498a94a2ea9eb10fce12f249f94
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-19 19:16:09 +0100

    textfield now hast onBlurr flag

--- server/appV2/src/lib/assets/cast/Textfield.svelte
+++ server/appV2/src/lib/assets/cast/Textfield.svelte
@@ -1,7 +1,11 @@
 <script>
-    let { placeholdertxt = "", value = $bindable(""), width=100, height=50} = $props();
-    
+    let { placeholdertxt = "", value = $bindable(""), width=100, height=50, onBlur=()=>{}} = $props();
 
+function test(){
+    console.log("try to trigger save flag");
+    onBlur();
+    console.log("triggered");
+}
 </script>
 
-<input class="flex shrink-0 p-3 text-base" style="width: {width}px; height: {height}px;" placeholder={placeholdertxt.toString()} bind:value/>
+<input class="flex shrink-0 p-3 text-base" style="width: {width}px; height: {height}px;" placeholder={placeholdertxt.toString()} bind:value onblur={()=>test()}/>
