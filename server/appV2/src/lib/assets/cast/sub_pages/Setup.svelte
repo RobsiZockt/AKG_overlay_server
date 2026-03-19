d70b119567b57e84e6047a0a3757613d4d0c3a33
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-19 17:11:29 +0100

    made the UI a bit better

--- server/appV2/src/lib/assets/cast/sub_pages/Setup.svelte
+++ server/appV2/src/lib/assets/cast/sub_pages/Setup.svelte
@@ -4,6 +4,8 @@
     import { teamdata } from "$lib/stores/TeamData";
   import { onMount } from "svelte";
   import Textfield from "../Textfield.svelte";
+  import TimeField from "../TimeField.svelte";
+  import BoxText from "$lib/assets/wrapper/BoxText.svelte";
 
     onMount(()=> {teamdata.load();casters.load()});
     let first_team = $state(0);
@@ -11,22 +13,33 @@
 </script>
 
 <div class="flex flex-col w-full h-full items-center">
-<p>SELECT TEAMS</p>
-    <div class="flex w-[80%]">
-        <DropdownMenu list={$teamdata} bind:value={first_team} nulltext="First named team" ></DropdownMenu>
-        <DropdownMenu list={$teamdata} bind:value={second_team} nulltext="Second named team" ></DropdownMenu>
-    </div>
-    <p>Caster Setup</p>
-    <div class=" w-[80%] h-fit">
+<BoxText title="Select Teams">
+  <div class="flex items-center w-[80%] gap-3">
+    <DropdownMenu list={$teamdata} bind:value={first_team} nulltext="First named team" width={300} ></DropdownMenu>
+    <DropdownMenu list={$teamdata} bind:value={second_team} nulltext="Second named team" width={300} ></DropdownMenu>
+  </div>
+</BoxText>
+<BoxText title="Caster Setup">
+  <div class=" w-[80%] h-fit">
     {#if casters!=null}
-        {#each $casters as caster}
-            <div class="flex w-full h-fit">
-                <Textfield placeholdertxt="Name" bind:value={caster.name}></Textfield>
-                <Textfield placeholdertxt="Social name" bind:value={caster.social}></Textfield>
-                <Textfield placeholdertxt="social ico" bind:value={caster.social_ico}></Textfield>
-            </div>
-        {/each}
+      {#each $casters as caster}
+        <div class="flex w-full gap-1 h-fit pb-3">
+				<p>Caster: </p>
+          <Textfield placeholdertxt="Name" bind:value={caster.name} width={300}></Textfield>
+          <Textfield placeholdertxt="Social name" bind:value={caster.social} width={300}></Textfield>
+          <Textfield placeholdertxt="social ico" bind:value={caster.social_ico}></Textfield>
+        </div>
+      {/each}
     {/if}
-    </div>
-
+  </div>
+</BoxText>
+<BoxText title="Timer Setup">
+  <div class="flex">
+ 		<p>Start Time:</p>
+    <TimeField hours="" minutes=""></TimeField>
+ 	</div>
+</BoxText>
+<BoxText title="Ruleset (very WIP)">
+	<p>The Ruleset system will come soonTM</p>
+</BoxText>
 </div>
