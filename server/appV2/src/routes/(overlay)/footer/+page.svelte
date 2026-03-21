999cb030a29fe597e8854c028656cb11f7e5df55
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-21 23:40:35 +0100

    created a Footer and infint scroling text copmonent

--- server/appV2/src/routes/(overlay)/footer/+page.svelte
+++ server/appV2/src/routes/(overlay)/footer/+page.svelte
@@ -1,7 +1,42 @@
 <script>
-    import PoweredBy from "$lib/assets/PoweredBy.svelte";
+  import InfiniteScrollText from "$lib/assets/InfiniteScrollText.svelte";
+import akg_logo from "$lib/assets/logos/akgaming_logo.png"
+import soloplan from "$lib/assets/logos/soloplan-square.svg"
+import uniliga from "$lib/assets/logos/Uniliga-TeilnehmerLogo-weiss.png"
+import { rot_text } from "$lib/stores/rottext";
+  import { onDestroy, onMount } from "svelte";
+
+  let toptext=$state("BUILDING DELAY");
+
+onMount(()=>rot_text.load());
+
+
+
+function rmdtxt(){
+
+    const n = Math.floor(Math.random()*($rot_text.length - 1))+1;
+    toptext = $rot_text[n].toUpperCase();
+    console.log(toptext);
+}
+
+const rotate = setInterval(rmdtxt,7000);
+onDestroy(()=>clearInterval(rotate));
+
 </script>
 
-<div class="h-[100vh] w-full items-end justify-center p-4 flex">
-<PoweredBy></PoweredBy>
+<div class="flex w-full h-[100vh] items-end">
+<div class="flex w-full h-[177px] bg-[#222222] p-2">
+<div class="flex border-2  w-full h-full">
+    <img class="pr-1" src={akg_logo} alt=""/>
+    <div class="w-[4px] h-full bg-black"></div>
+    <div class="w-[2000px] flex flex-col">
+        <span class="text-white text-7xl font-bold pb-1 pr-2">{toptext}</span>
+        <div class="w-full h-[2px] bg-black"></div>
+        <InfiniteScrollText content={["DATA1","DATA2","DATA 3 WITH SPACES","asdsdhfafgniajsdfghijosadfhaisdfhaiosdfjioasjdfafisdjafsdihu"]} speed={60}></InfiniteScrollText>
+    </div>
+    <div class="w-[4px] h-full bg-black"></div>
+    <img class="pl-1" src={soloplan} alt=""/>
+    <img class="p-[15px]" src={uniliga} alt=""/>
+</div>
+</div>
 </div>
