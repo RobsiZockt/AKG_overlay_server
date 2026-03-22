4ae13e8dac1705392c903f4d98d6097cbc4b6fb3
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-03-22 19:36:11 +0100

    added an end screen

---
+++ server/appV2/src/routes/(overlay)/footer/end/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Footer from "$lib/assets/Footer.svelte";
+</script>
+
+<div class="flex w-full h-[100vh] items-end">
+<Footer cd_mode={"end"} cd_text={""} scrolltext={["Der AKgaming ev bedankt sich fürs zuschauen und wünscht einen schönen abend"]}></Footer>
+</div> 
