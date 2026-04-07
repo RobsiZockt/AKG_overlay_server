f0ef0a558aed534273777451336f1cd4728788a4
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-04-07 16:46:34 +0200

    HOTFIX: VS header now reads matchup correctly

--- server/appV2/src/lib/assets/VSHeader.svelte
+++ server/appV2/src/lib/assets/VSHeader.svelte
@@ -1,22 +1,23 @@
 <script>
-let matchupData = $props();
+let {matchData} = $props();
+$effect(()=>console.log(matchData));
 
 </script>
 
     <div class="relative flex h-[128px] w-[2000px] items-start justify-center z-2 bg-[#1e1e1e] rounded-lg border-2 border-[#ffaaaa]">
         <div class="flex w-[45%] h-full items-center justify-start pr-1">
-            <img src={matchupData.blue_logo} alt="" class="h-[96px] object-contain"/>
-            <span class="text-white text-7xl pl-9">{matchupData.blue}</span>
+            <img src={matchData.blue_logo} alt="" class="h-[96px] object-contain"/>
+            <span class="text-white text-7xl pl-9">{matchData.blue}</span>
 
         </div>
         <div class="flex w-[6%] h-full items-center justify-center px-2">
-            <span class="text-white text-7xl ">{matchupData.blue_score}</span>
+            <span class="text-white text-7xl ">{matchData.blue_score}</span>
             <span class="text-white text-7xl px-1">:</span>
-            <span class="text-white text-7xl ">{matchupData.red_score}</span>
+            <span class="text-white text-7xl ">{matchData.red_score}</span>
         </div>
         <div class="flex w-[45%] h-full items-center justify-end pl-1">
 
-            <span class="text-white text-7xl pr-9">{matchupData.red}</span>
-            <img src={matchupData.red_logo} alt="" class="h-[96px] object-contain"/>
+            <span class="text-white text-7xl pr-9">{matchData.red}</span>
+            <img src={matchData.red_logo} alt="" class="h-[96px] object-contain"/>
         </div>
     </div>
