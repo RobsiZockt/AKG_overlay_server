4cafba93f10dd76f158ba0b8dbaba9f965f3771c
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-04-16 20:29:16 +0200

    HOTFIX: fixed soloplan rendering

--- server/appV2/src/routes/(overlay)/sponsors/soloplan/+page.svelte
+++ server/appV2/src/routes/(overlay)/sponsors/soloplan/+page.svelte
@@ -5,8 +5,8 @@ import soloplan from "$lib/assets/logos/Soloplan_wide.png";
 const bg = $derived(page.url.searchParams.get('bg'));
 
 let conf = $state("");
-if(bg=="black") conf = "m-0 p-0 w-[1024px] h-[343px] overflow-hidden bg-[#282828ee]";
-if(bg=="nobg")  conf = "m-0 p-0 w-[1024px] h-[343px] overflow-hidden ";
+if(bg=="black") conf = "m-0 p-0 flex w-[1024px] h-[343px] overflow-hidden justify-center bg-[#282828ee]";
+if(bg=="nobg")  conf = "m-0 p-0 flex w-[1024px] h-[343px] overflow-hidden justify-center";
 </script>
 
 <div class={conf}>
