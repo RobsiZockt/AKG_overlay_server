b6f92a5874bb2e4a7b3441dd611db5705ed6c12b
robsizockt <94533157+RobsiZockt@users.noreply.github.com>
2026-05-15 18:59:39 +0200

    added high contrast sponsor

--- server/appV2/src/routes/(overlay)/sponsors/soloplan/+page.svelte
+++ server/appV2/src/routes/(overlay)/sponsors/soloplan/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 import { page } from "$app/state";
 import soloplan from "$lib/assets/logos/Soloplan_wide.png";
+import soloplan_hc from "$lib/assets/logos/Soloplan_wide_HC.png"
 
 const bg = $derived(page.url.searchParams.get('bg'));
+const hc = $derived(page.url.searchParams.get('hc'));
 
 let conf = $state("");
 if(bg=="black") conf = "m-0 p-0 flex w-[1024px] h-[343px] overflow-hidden justify-center bg-[#282828ee]";
@@ -10,5 +12,5 @@ if(bg=="nobg")  conf = "m-0 p-0 flex w-[1024px] h-[343px] overflow-hidden justif
 </script>
 
 <div class={conf}>
-    <img src={soloplan} alt="" class="h-[95%] w-[90%] bg-contain bg-no-repeat "/>
+    <img src={hc=="true"?soloplan_hc:soloplan} alt="" class="h-[95%] w-[90%] bg-contain object-contain bg-no-repeat "/>
 </div>
